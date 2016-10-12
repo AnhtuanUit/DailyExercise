ddb71d8885e8c3a87709f63055fc4fb595eaf557
AnhtuanUit <AnhtuanUit@users.noreply.github.com>
2016-10-12 14:03:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var debug = require('debug')('app');
 var app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 9006);
 
 var server = app.listen(app.get('port'), function() {
     var socketIO = require('./socketio')(server);
